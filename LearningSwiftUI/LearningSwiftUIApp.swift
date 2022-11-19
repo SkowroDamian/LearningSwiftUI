035cea240a5f912556eb2ad03f6d25b4ccf7b891
damian <skowronskid@icloud.com>
2022-11-19 23:02:28 +0100

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Damian Skowroński on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
